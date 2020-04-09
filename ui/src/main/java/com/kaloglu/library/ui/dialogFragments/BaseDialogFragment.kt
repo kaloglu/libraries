6c35abe677f7ce12ff090705fecbea49183f74b5
kaloglu <cihan@kaloglu.com>
2020-04-09 18:30:33 +0300

    Set dialog fragment structure

--- ui/src/main/java/com/kaloglu/library/ui/dialogFragments/BaseDialogFragment.kt
+++ ui/src/main/java/com/kaloglu/library/ui/dialogFragments/BaseDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.annotation.ContentView
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.DialogFragment
@@ -11,30 +12,39 @@ import com.kaloglu.library.ui.BaseActivity
 import com.kaloglu.library.ui.interfaces.DialogFragmentLifecycle
 
 abstract class BaseDialogFragment @ContentView constructor(
-        @LayoutRes internal open val resourceLayoutId: Int = 0
+    @LayoutRes internal open val resourceLayoutId: Int = 0
 ) : DialogFragment(), DialogFragmentLifecycle {
 
     override lateinit var containerView: View
 
     override val activity by lazy { getActivity() as BaseActivity }
     override val application by lazy { activity.application }
-    override val dialogTitle: String? = null
 
     override fun initUserInterface(savedInstanceState: Bundle?) {
-        dialog?.let {
-            it.setOnShowListener {
-                dialogTitle?.let { dialog?.setTitle(it) }
-                dialog?.window?.setLayout(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT
-                )
+        dialog?.let { dialog ->
+            dialog.setOnShowListener {
+                dialog.window?.let {
+                    setDialogStyle(it)
+                }
+                dialog.setCancelable(false)
             }
-            initUserInterface(it)
+            initUserInterface(dialog)
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, parent: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(resourceLayoutId, parent)
+    override fun setDialogStyle(dialogWindow: Window) {
+        dialogWindow.setLayout(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        parent: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(resourceLayoutId, parent)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         containerView = view
--- ui/src/main/java/com/kaloglu/library/ui/interfaces/DialogFragmentLifecycle.kt
+++ ui/src/main/java/com/kaloglu/library/ui/interfaces/DialogFragmentLifecycle.kt
@@ -1,11 +1,11 @@
 package com.kaloglu.library.ui.interfaces
 
 import android.app.Dialog
+import android.view.Window
 
 interface DialogFragmentLifecycle : ViewLifecycle {
 
-    val dialogTitle: String?
-
     fun initUserInterface(dialog: Dialog) = Unit
 
+    fun setDialogStyle(dialogWindow: Window)
 }
