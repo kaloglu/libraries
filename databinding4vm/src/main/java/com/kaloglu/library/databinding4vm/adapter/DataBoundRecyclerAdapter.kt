1d7fc99b13f3fd5dc38ad16afe9f38b373826f35
kaloglu <cihan@kaloglu.com>
2020-07-12 16:53:43 +0300

    Rename BaseRecyclerAdapter to BaseListAdapter
    Implement PagedListAdapter with BasePagedListAdapter

--- databinding4vm/src/main/java/com/kaloglu/library/databinding4vm/adapter/DataBoundRecyclerAdapter.kt
+++ databinding4vm/src/main/java/com/kaloglu/library/databinding4vm/adapter/DataBoundRecyclerAdapter.kt
@@ -1,34 +1,18 @@
 package com.kaloglu.library.databinding4vm.adapter
 
-import android.view.ViewGroup
-import androidx.annotation.CallSuper
-import androidx.databinding.ViewDataBinding
-import com.kaloglu.library.ui.adapter.BaseRecyclerAdapter
 import com.kaloglu.library.ui.DiffItemSimpleCallback
 import com.kaloglu.library.ui.RecyclerItem
-import com.kaloglu.library.databinding4vm.inflateViewHolderBinding
+import com.kaloglu.library.ui.adapter.BaseListAdapter
 
+@Deprecated(
+    "Removal",
+    replaceWith = ReplaceWith(
+        "DataBoundListAdapter<RI>(\n" +
+                "    diffUtilCallback: DiffItemSimpleCallback<RI> = DiffItemSimpleCallback()\n" +
+                ")"
+    ), level = DeprecationLevel.ERROR
+)
 abstract class DataBoundRecyclerAdapter<RI>(
     diffUtilCallback: DiffItemSimpleCallback<RI> = DiffItemSimpleCallback()
-) : BaseRecyclerAdapter<RI, BoundViewHolder<RI>>(diffUtilCallback)
-        where RI : RecyclerItem {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        BoundViewHolder<RI>(
-            onCreateBinding(parent, viewType)
-        )
-
-    @CallSuper
-    open fun onCreateBinding(parent: ViewGroup, viewType: Int) =
-        parent.inflateViewHolderBinding(viewType)
-
-    override fun onBindViewHolder(holder: BoundViewHolder<RI>, position: Int) {
-        holder.setBindingVariable = this::setBindVariable
-        super.onBindViewHolder(holder, position)
-    }
-
-    /**
-     * Should set viewDataBinding.setVariable(BR.item, recylerItem)
-     * */
-    abstract fun setBindVariable(viewDataBinding: ViewDataBinding, recylerItem: RI)
-}
+) : BaseListAdapter<RI, BoundViewHolder<RI>>(diffUtilCallback)
+        where RI : RecyclerItem
---
+++ ui/src/main/java/com/kaloglu/library/ui/adapter/BasePagedListAdapter.kt
@@ -0,0 +1,54 @@
+package com.kaloglu.library.ui.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.ListAdapter
+import com.kaloglu.library.ui.DiffItemSimpleCallback
+import com.kaloglu.library.ui.RecyclerItem
+import com.kaloglu.library.ui.interfaces.ClickableRecyclerItemAdapter
+
+abstract class BasePagedListAdapter<RI, VH>(
+    diffUtilCallback: DiffItemSimpleCallback<RI> = DiffItemSimpleCallback()
+) : ListAdapter<RI, VH>(AsyncDifferConfig.Builder<RI>(diffUtilCallback).build()),
+    ClickableRecyclerItemAdapter<RI>
+        where RI : RecyclerItem, VH : BaseViewHolder<RI> {
+
+    override var onItemClick: ((RI, Int) -> Unit)? = null
+    override var onItemLongClick: (RI, Int) -> Boolean = { _, _ -> false }
+    override var onViewClick: ((RI, View, Int) -> Unit)? = null
+    override var onViewLongClick: (RI, View, Int) -> Boolean = { _, _, _ -> false }
+
+    internal val viewHolders: MutableList<VH> = mutableListOf()
+
+    abstract override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH
+
+    private fun VH.addLifeCycleList() = this.apply {
+        viewHolders.add(this)
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) =
+        holder
+            .addLifeCycleList()
+            .setClicks(onViewClick, onViewLongClick)
+            .bind(getItem(position), onItemClick, onItemLongClick)
+
+    override fun onViewAttachedToWindow(holder: VH) {
+        super.onViewAttachedToWindow(holder)
+        holder.onAttach()
+    }
+
+    override fun onViewDetachedFromWindow(holder: VH) {
+        super.onViewDetachedFromWindow(holder)
+        holder.onDetach()
+    }
+
+    override fun getItemViewType(position: Int) = getItem(position)?.layoutId ?: 0
+
+    open fun onLifecycleDestroyed() {
+        viewHolders.forEach {
+            it.onDestroyed()
+        }
+    }
+
+}
