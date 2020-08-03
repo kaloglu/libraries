ac0e36362d515ad3860585474c029aaf8a5b7a77
kaloglu <cihan@kaloglu.com>
2020-08-03 15:50:48 +0300

    Add new ViewModelFactory

--- viewmodel/src/main/java/com/kaloglu/library/viewmodel/BaseViewModelFactory.kt
+++ viewmodel/src/main/java/com/kaloglu/library/viewmodel/BaseViewModelFactory.kt
@@ -1,5 +1,6 @@
 package com.kaloglu.library.viewmodel
 
+import androidx.annotation.CallSuper
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -7,17 +8,32 @@ import com.kaloglu.library.ui.BaseApplication
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
-@Suppress("MemberVisibilityCanBePrivate")
-open class BaseViewModelFactory<A>(val application: A, val lifecycle: Lifecycle) :
-    ViewModelProvider.AndroidViewModelFactory(application)
-        where A : BaseApplication {
+open class ViewModelFactory<A, BVM>(
+    private val application: A,
+    private val lifecycle: Lifecycle,
+    private val viewModel: BVM
+) : ViewModelProvider.AndroidViewModelFactory(application)
+        where A : BaseApplication, BVM : BaseViewModel<*, *> {
 
     @Suppress("UNCHECKED_CAST")
     override fun <VM : ViewModel?> create(modelClass: Class<VM>) =
         when {
-            BaseViewModel::class.java.isAssignableFrom(modelClass) ->
-                modelClass.getConstructor(application::class.java).newInstance(application)
+            BaseViewModel::class.java.isAssignableFrom(modelClass) -> {
+                createViewModel(
+                    modelClass
+                        .getConstructor(application::class.java)
+                        .newInstance(application) as BaseViewModel<*, *>
+                ) as VM
+            }
             else ->
                 super.create(modelClass)
         }
+
+    @CallSuper
+    open fun createViewModel(createdViewModel: BaseViewModel<*, *>) =
+        createdViewModel.apply {
+            lifecycle.addObserver(this)
+            attachViewModel(viewModel)
+        }
+
 }
